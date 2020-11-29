fd391512ffbe84b0573a217bed7872895a6e69c8
ruchit.raj <ruchit.raj@robosoftin.com>
2020-11-29 22:58:09 +0530

    Added insert at any index method

--- LinkedList.js
+++ LinkedList.js
@@ -38,6 +38,32 @@ class LinkedList {
 
     // Insert at index
 
+    insertAt(data, index) {
+        // if index is out of scope
+        if(index > 0 && index > this.size) {
+            console.log("Invalid argument")
+        }
+        // if first index
+        else if(index === 0) {
+            return this.head = new Node(data, this.head)
+            return;
+        }
+       else if(index === this.size) {
+            this.insertLast(data);
+        }
+        else {
+            let node = new Node(data);
+            let previous, current;
+            current = this.head;
+            for(let i = 0; i < index - 1 ; i++) {
+                current = current.next;
+            }
+            node.next = current.next;
+            current.next = node;
+            this.size++;
+        }
+    }
+
     // Get at index
 
     // Remove at index
@@ -57,8 +83,9 @@ class LinkedList {
 
 const ll = new LinkedList();
 
-ll.insertFirst(100)
-ll.insertFirst(200)
 ll.insertFirst(300)
+ll.insertFirst(200)
+ll.insertFirst(100)
 ll.insertLast(400)
+ll.insertAt(500,4)
 ll.printListData();
